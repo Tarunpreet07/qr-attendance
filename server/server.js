1de126dbe3e370f42c113807d3a427be1411f513
Tarunpreet07 <kaurtarunpreet2@gmail.com>
2025-08-02 20:21:15 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config();
 connectDB();
