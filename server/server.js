91243b1ef5941c054745e90a44e8ba8205450c39
Tarunpreet07 <kaurtarunpreet2@gmail.com>
2025-08-02 20:07:18 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/sessions', require('./routes/session'));
 app.use('/api/attendance', require('./routes/attendance'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
