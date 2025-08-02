b2c79d641a0f7ad3efc2d0d7b84343f914938636
Tarunpreet07 <kaurtarunpreet2@gmail.com>
2025-08-02 20:34:56 +0530

    fifth

--- server/server.js
+++ server/server.js
@@ -20,13 +20,6 @@ app.use('/api/sessions', require('./routes/session'));
 console.log('Loading attendance route...');
 app.use('/api/attendance', require('./routes/attendance'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
